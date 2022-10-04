8f8619afd9e959cbd0390b7576168cd80cd680bb
olehb <legowspace@gmail.com>
2022-10-04 22:31:32 +0300

    Complete hw

--- src/ua/hillelit/homeworks/main/Main.java
+++ src/ua/hillelit/homeworks/main/Main.java
@@ -1,7 +1,53 @@
 package ua.hillelit.homeworks.main;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        ValueCalculator vc = new ValueCalculator(ValueCalculator.doCalc(arrayCreator()));
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread1 is working");
+                vc.setFirstHalf(vc.changeArray(vc.getFirstHalf()));
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread2 is working");
+                vc.setSecondHalf(vc.changeArray(vc.getSecondHalf()));
+            }
+        });
+        try{
+            thread1.start();
+            thread1.join();
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException main!");
+        }
+        try{
+            thread2.start();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException main!");
+        }
+        vc.getFullArray(vc.getFirstHalf(), vc.getSecondHalf());
+        long finish = System.currentTimeMillis();
+        System.out.println("Время выполнения: " + (finish - start) + " миллисекунд");
+
+    }
+
+    public static double[] arrayCreator() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива (не меньше 1 000 000)");
+        int arraySize = scanner.nextInt();
+        if (arraySize < 1000000) {
+            System.out.println("Размер массива должен быть не меньше 1 000 000!");
+            arrayCreator();
+        }
+        double[] array = new double[arraySize];
+        return array;
     }
 }
--- src/ua/hillelit/homeworks/main/ValueCalculator.java
+++ src/ua/hillelit/homeworks/main/ValueCalculator.java
@@ -4,29 +4,104 @@ import java.awt.event.MouseAdapter;
 import java.util.Random;
 
 public class ValueCalculator {
-    double array[];
-    int arraySize;
-    int halfArraySize;
+    private int arraySize;
+    private double array[];
+    private int halfArraySize;
+    private double firstHalf[];
+    private double secondHalf[];
 
 
+    public ValueCalculator(double[] array) {
+        this.array = array;
+        this.arraySize = array.length;
+        this.halfArraySize = array.length/2;
+        this.firstHalf = this.getFirstHalf(array);
+        this.secondHalf = this.getSecondHalf(array);
+    }
 
-    private void doCalc(double[] array) {
-        long start = System.currentTimeMillis();
-        double a1[] = new double[0];
-        double a2[] = new double[0];
-        for (double a : array) {
-            a = Math.random();
+    static double[] doCalc(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+        return array;
+    }
+
+    public double[] printArr(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+        return array;
+    }
+
+
+    double[] getFirstHalf(double[] array) {
+        double arr[] = new double[halfArraySize];
+        System.arraycopy(array, 0, arr, 0, halfArraySize);
+        for (double a : arr) {
             System.out.print(a + "\t");
         }
         System.out.println();
-        System.arraycopy(array, 0, a1, 1, halfArraySize);
-        System.arraycopy(array, halfArraySize, a2, 0, halfArraySize);
-        for (double a : a1) {
+        return arr;
+    }
+
+    double[] getSecondHalf(double[] array) {
+        double arr[] = new double[halfArraySize];
+        System.arraycopy(array, halfArraySize, arr, 0, halfArraySize);
+        for (double a : arr) {
             System.out.print(a + "\t");
         }
         System.out.println();
-        for (double a : a2) {
+        return arr;
+    }
+
+    double[] changeArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            System.out.print(arr + "\t");
+        }
+        System.out.println();
+        return arr;
+    }
+
+    double[] getFullArray(double[] arr1, double[] arr2) {
+        double arr[] = new double[arraySize];
+        System.arraycopy(arr1, 0, arr, 0, halfArraySize);
+        System.arraycopy(arr2, 0, arr, halfArraySize, halfArraySize);
+        for (double a : arr) {
             System.out.print(a + "\t");
         }
+        System.out.println();
+        return arr;
+    }
+
+    public double[] getArray() {
+        return array;
+    }
+
+    public double[] getFirstHalf() {
+        return firstHalf;
+    }
+
+    public void setFirstHalf(double[] firstHalf) {
+        this.firstHalf = firstHalf;
+    }
+
+    public double[] getSecondHalf() {
+        return secondHalf;
+    }
+
+    public void setSecondHalf(double[] secondHalf) {
+        this.secondHalf = secondHalf;
+    }
+
+    public int getArraySize() {
+        return arraySize;
+    }
+
+    public void setArray(double[] array) {
+        this.array = array;
     }
 }
